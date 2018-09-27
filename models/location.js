c58fcc098635c9c322953dbac351817bbb8958a5
JonathanLin0707 <35053296+JonathanLin0707@users.noreply.github.com>
2018-09-27 13:31:17 +0800

    add location table

---
+++ models/location.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('location', new Schema({
+    name: String,
+    locationid: String,
+    user: [String]
+}), 'location');
