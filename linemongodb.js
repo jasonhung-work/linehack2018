12bc7ddb80fb4e70a9f0742b628df6914ee4dcc6
Jonathan Lin <takashi710707@hotmail.com>
2018-09-29 13:43:18 +0800

    add get_shuangjious
    加入取得所有揪團資料

--- linemongodb.js
+++ linemongodb.js
@@ -136,6 +136,23 @@ var linemongodb = function () {
         });
     }
 
+    //取得所有揪團資訊
+    this.get_shuangjious = function (callback) {
+        console.log('get_shuangjious');
+
+        this.ShuangJiou.find(function (err, shuangjious) {
+            if (err) {
+                callback(err);
+            }
+            else {
+                console.log('ShuangJiou get successfully');
+                if (shuangjious)
+                    callback(null, shuangjious);
+                else
+                    callback(null, null);
+            }
+        });
+    }
 
     //Host
     //建立爽主資訊
@@ -579,6 +596,19 @@ var linemongodb = function () {
                     callback(null, null);
             }
         });
+    }
+
+    //計算距離(公尺)
+    this.getdistance = function (lat1, lng1, lat2, lng2) {
+        var radLat1 = lat1 * Math.PI / 180.0;
+        var radLat2 = lat2 * Math.PI / 180.0;
+        var a = radLat1 - radLat2;
+        var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+        var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+            Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+        s = s * 6378.137;// EARTH_RADIUS;
+        s = Math.round(s * 10000) / 10000;
+        return s * 1000;
     }
 }
 
